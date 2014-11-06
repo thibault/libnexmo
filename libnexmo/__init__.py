f7ba08bfd8c863c85bf6e58288846503e93f64dd
Thibault Jouannic <thibault@miximum.fr>
2014-11-06 18:27:44 +0100

    init module import

---
+++ libnexmo/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from libnexmo.base import Nexmo
+from libnexmo.response import NexmoResponse
+
+
+__all__ = ['Nexmo', 'NexmoResponse']
+
+__version__ = '1.0.0'
