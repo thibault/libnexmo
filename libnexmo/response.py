dbc4d73e4d38586d7d5bea79b89aa973f22b0580
Thibault Jouannic <thibault@miximum.fr>
2014-11-07 12:35:08 +0100

    Nexmo response

--- libnexmo/response.py
+++ libnexmo/response.py
@@ -2,6 +2,8 @@
 
 from __future__ import unicode_literals
 
+from decimal import Decimal as D
+
 
 class NexmoResponse(object):
     """A convenient wrapper to manipulate the Nexmo json response.
@@ -30,4 +32,29 @@ class NexmoResponse(object):
     """
 
     def __init__(self, json_data):
-        pass
+        self.messages = [NexmoMessage(data) for data in json_data['messages']]
+        self.message_count = len(self.messages)
+        self.total_price = sum(msg.message_price for msg in self.messages)
+        self.remaining_balance = min(msg.remaining_balance for msg in self.messages)
+
+
+class NexmoMessage(object):
+    """A wrapper to manipulate a single `message` entry in a Nexmo response.
+
+    When a text messages is sent in several parts, Nexmo will return a status
+    for each and everyone of them.
+
+    The class does nothing more than simply wrapping the json data for easy
+    access.
+
+    """
+    def __init__(self, json_data):
+        data = {
+            'to': json_data['to'],
+            'message_id': json_data['message-id'],
+            'status': int(json_data['status']),
+            'remaining_balance': D(json_data['remaining-balance']),
+            'message_price': D(json_data['message-price']),
+            'network': json_data['network']
+        }
+        self.__dict__.update(data)
--- tests/test_send_sms.py
+++ tests/test_send_sms.py
@@ -2,6 +2,8 @@
 
 from __future__ import unicode_literals
 
+from decimal import Decimal as D
+
 import responses
 
 from libnexmo import Nexmo
@@ -37,3 +39,73 @@ def test_response_type():
         content_type='application/json')
     response = nexmo.send_sms(FRM, TO, MSG)
     assert isinstance(response, NexmoResponse)
+
+
+@responses.activate
+def test_response_single_message():
+    """The response is correctly populated with a single message."""
+    responses.add(
+        responses.POST,
+        'https://rest.nexmo.com/sms/json',
+        body='''{
+            "message-count":"1",
+            "messages":[{
+                "to":"33123456789",
+                "message-id":"0200000044CF5208",
+                "status":"0",
+                "remaining-balance":"6.40600000",
+                "message-price":"0.04500000",
+                "network":"20823"
+            }]
+        }''',
+        status=200,
+        content_type='application/json')
+    response = nexmo.send_sms(FRM, TO, MSG)
+
+    assert response.total_price == D('0.045')
+    assert response.message_count == 1
+    assert response.remaining_balance == D('6.406')
+    assert response.messages[0].message_id == '0200000044CF5208'
+    assert response.messages[0].message_price == D('0.045')
+
+
+@responses.activate
+def test_response_multiple_messages():
+    """The response is correctly populated with multiples messages."""
+    responses.add(
+        responses.POST,
+        'https://rest.nexmo.com/sms/json',
+        body='''{
+            "message-count":"3",
+            "messages":[{
+                "status":"0",
+                "message-id":"00000124",
+                "to":"33123456789",
+                "remaining-balance":"1.10",
+                "message-price":"0.05",
+                "network":"23410"
+            },{
+                "status":"0",
+                "message-id":"00000125",
+                "to":"33123456789",
+                "remaining-balance":"1.05",
+                "message-price":"0.05",
+                "network":"23410"
+            },{
+                "status":"0",
+                "message-id":"00000126",
+                "to":"33123456789",
+                "remaining-balance":"1.00",
+                "message-price":"0.05",
+                "network":"23410"
+            }]
+        }''',
+        status=200,
+        content_type='application/json')
+    response = nexmo.send_sms(FRM, TO, MSG)
+
+    assert response.total_price == D('0.15')
+    assert response.message_count == 3
+    assert response.remaining_balance == D('1')
+    assert response.messages[1].message_id == '00000125'
+    assert response.messages[1].message_price == D('0.05')
